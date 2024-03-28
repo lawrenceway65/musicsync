e5f2bb72f1b399e4988bfd0bad3c81c5cabb3965
lawrenceway65 <lawrenceway@yahoo.co.uk>
2024-03-28 22:50:53 +0000

    Manage clean up on dest

--- main.py
+++ main.py
@@ -3,16 +3,42 @@
 import os
 import shutil
 
-# source_root = "C:\\Users\\lawre\\Local\\MusicTest"
-source_root = "C:\\Users\\lawre\\OneDrive\\Music"
-# dest_root = "C:\\Users\\lawre\\Local\\MusicTestSync"
-dest_root = "D:\\Music"
+source_root = "C:\\Users\\lawre\\Local\\MusicTest"
+#source_root = "C:\\Users\\lawre\\OneDrive\\Music"
+dest_root = "C:\\Users\\lawre\\Local\\MusicTestSync"
+#dest_root = "D:\\Music"
 
 
 def music_sync():
+    # Remove directories/files on destination that are not present on source
+    items_removed = 0
+    for (root, dirlist, filelist) in os.walk(dest_root, topdown=True):
+        dirs = [n for n in dirlist]
+        for i in dirs:
+            dest_dir = root + "\\" + i
+            source_dir = source_root + dest_dir[len(dest_root):len(dest_dir)]
+            # Check if source dir exists
+            if not os.path.exists(source_dir):
+                print("Delete directory: " + dest_dir)
+                shutil.rmtree(dest_dir)
+                items_removed += 1
+                # No need to check files - rmtree will have removed them
+                continue
+            files = os.scandir(dest_dir)
+            for n in files:
+                if n.is_file():
+                    dest_filespec = dest_dir + "\\" + n.name
+                    source_filespec = source_root + dest_filespec[len(dest_root):len(dest_filespec)]
+#                    print(source_filespec)
+                    if not os.path.exists(source_filespec):
+                        print("Delete file: " + dest_filespec)
+                        os.remove(dest_filespec)
+                        items_removed += 1
+    print(str(items_removed) + " files/folders removed on destination")
+
+    # Copy any file either not present on dest or present but different size
     dir_count = 0
     diff_count = 0
-    # Use a breakpoint in the code line below to debug your script.
     for (root, dirlist, filelist) in os.walk(source_root, topdown=True):
         dirs = [n for n in dirlist]
         for i in dirs:
@@ -28,32 +54,22 @@ def music_sync():
                 if n.is_file():
                     source_filespec = source_dir + "\\" + n.name
                     source_size = os.path.getsize(source_filespec)
-#                    print(source_filespec)
-                    leaf = source_filespec[len(source_root):len(source_filespec)]
-#                    print(leaf)
-                    dest_filespec = dest_root + leaf
-#                    print(dest_filespec)
+                    dest_filespec = dest_root + source_filespec[len(source_root):len(source_filespec)]
                     update = False
                     if os.path.exists(dest_filespec):
                         dest_size = os.path.getsize(dest_filespec)
-#                        print(str(dest_size) + "\t" + str(dest_size))
                         if source_size != dest_size:
-                            print(source_filespec + ": No size match")
+                            reason = "changed"
                             update = True
                             diff_count += 1
                     else:
-                        print(source_filespec + ": No file")
+                        reason = "new"
                         update = True
                         diff_count += 1
-#                    if update:
-#                        print("Copy file: " + source_filespec)
+                    if update:
+                        print("Copy " + reason + " file: " + source_filespec)
                         shutil.copy(source_filespec, dest_filespec)
     print(str(dir_count) + " directories checked, " + str(diff_count) + " files updated")
-
-# Changes : create directories, reverse check
-#        files = [n for n in filelist]
-#        for i in files:
-#            print(i)
 
 
 # Press the green button in the gutter to run the script.
