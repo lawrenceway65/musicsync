e57d84b74774e2b129ee9717df118a8cca9d12bb
lawrenceway65 <lawrenceway@yahoo.co.uk>
2024-03-28 18:23:06 +0000

    Manage directory creation

--- main.py
+++ main.py
@@ -3,30 +3,35 @@
 import os
 import shutil
 
-# source_dir = "C:\\Users\\lawre\\Local\\MusicTest"
-source_dir = "C:\\Users\\lawre\\OneDrive\\Music"
-# dest_dir = "C:\\Users\\lawre\\Local\\MusicTestSync"
-dest_dir = "D:\\Music"
+# source_root = "C:\\Users\\lawre\\Local\\MusicTest"
+source_root = "C:\\Users\\lawre\\OneDrive\\Music"
+# dest_root = "C:\\Users\\lawre\\Local\\MusicTestSync"
+dest_root = "D:\\Music"
 
 
 def music_sync():
     dir_count = 0
     diff_count = 0
     # Use a breakpoint in the code line below to debug your script.
-    for (root, dirlist, filelist) in os.walk(source_dir, topdown=True):
+    for (root, dirlist, filelist) in os.walk(source_root, topdown=True):
         dirs = [n for n in dirlist]
         for i in dirs:
             dir_count += 1
-            current_dir = root + "\\" + i
-            files = os.scandir(current_dir)
+            source_dir = root + "\\" + i
+            dest_dir = dest_root + source_dir[len(source_root):len(source_dir)]
+            # Check if dest dir exists
+            if not os.path.exists(dest_dir):
+                print("Create directory: " + dest_dir)
+                os.mkdir(dest_dir)
+            files = os.scandir(source_dir)
             for n in files:
                 if n.is_file():
-                    source_filespec = current_dir + "\\" + n.name
+                    source_filespec = source_dir + "\\" + n.name
                     source_size = os.path.getsize(source_filespec)
 #                    print(source_filespec)
-                    leaf = source_filespec[len(source_dir):len(source_filespec)]
+                    leaf = source_filespec[len(source_root):len(source_filespec)]
 #                    print(leaf)
-                    dest_filespec = dest_dir + leaf
+                    dest_filespec = dest_root + leaf
 #                    print(dest_filespec)
                     update = False
                     if os.path.exists(dest_filespec):
@@ -41,8 +46,9 @@ def music_sync():
                         update = True
                         diff_count += 1
 #                    if update:
-#                        shutil.copy(source_filespec, dest_filespec)
-    print(str(dir_count) + " directories checked, " + str(diff_count) + " differences")
+#                        print("Copy file: " + source_filespec)
+                        shutil.copy(source_filespec, dest_filespec)
+    print(str(dir_count) + " directories checked, " + str(diff_count) + " files updated")
 
 # Changes : create directories, reverse check
 #        files = [n for n in filelist]
